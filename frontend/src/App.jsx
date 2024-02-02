95d3af5615f25817a69df9fa1f06384f30285987
Sushmita Ghosh <pujaghosh251096@gmail.com>
2024-02-01 21:53:37 -0800

    [Setup FIles]

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import QuizMain from "./components/QuizMain/QuizMain";
+
+function App() {
+  return (
+    <div className="App">
+      <QuizMain />
+    </div>
+  );
+}
+
+export default App;
