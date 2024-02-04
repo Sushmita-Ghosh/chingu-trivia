df2185aa6d155afa760b7d051970b01176655edd
Sushmita Ghosh <pujaghosh251096@gmail.com>
2024-02-04 10:51:49 -0800

    [Chingu Trivia] Adding React Router Dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.scss";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import QuizQuestionContainer from "./components/QuizQuestionContainer/QuizQuestionContainer.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/quiz",
+    element: <QuizQuestionContainer />,
+  },
+  {
+    path: "*",
+    element: <div>Error</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
