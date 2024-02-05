46602c5ced16f47fb29d1c6e101a9f360629767e
Sushmita Ghosh <pujaghosh251096@gmail.com>
2024-02-04 21:47:57 -0800

    [Chingu Trivia] - Quiz Container Design

---
+++ frontend/src/components/QuizQuestionContainer/QuizQuestionContainer.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import "./QuizQuestionContainer.css";
+/* eslint-disable react/prop-types */
+const QuizQuestionContainer = () => {
+  const { state } = useLocation();
+
+  const { questions } = state;
+
+  const [currentQuestion, setCurrentQuestion] = useState(1);
+  const { question, answer, choices, id } = questions[currentQuestion];
+  console.log(Object.values(choices));
+  return (
+    <div className="quiz-question-container">
+      <div className="question-title">
+        <span className="active-question-number">
+          Question {currentQuestion}
+        </span>
+        <span className="total-question-number">/{questions.length}</span>
+      </div>
+      <h2 className="question">{question}</h2>
+
+      <div className="choices-cointainer">
+        {Object.values(choices).map((choice) => (
+          <div className="choice" key={choice}>
+            {choice}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuizQuestionContainer;
