58466a784019c8d75b1350eb0715e87dadda0d64
Sushmita Ghosh <pujaghosh251096@gmail.com>
2024-02-06 19:18:13 -0800

    [Chingu Trivia] Incorporating Outlet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import QuizMain from "./components/QuizMain/QuizMain";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <QuizMain />
+    <div className="app">
+      <Header />
+      <Outlet />
     </div>
   );
 }
