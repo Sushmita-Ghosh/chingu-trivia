0c2360fcdd106158da0b66edb5099713e84f37b3
Sushmita Ghosh <pujaghosh251096@gmail.com>
2024-02-11 04:13:25 -0800

    [Chingu Trivia] Header Component

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,26 @@
+import React from "react";
+import AuthContext from "../../context/authentication";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const { login } = React.useContext(AuthContext);
+  const user = localStorage.getItem("user");
+
   return (
     <div className="header">
-      <h1>Chingu Trivia</h1>
+      <Link className="link" to="/">
+        <h1>Chingu Trivia</h1>
+      </Link>
+      {user ? (
+        <Link className="link" to="/login">
+          <h1>{JSON.parse(user).data.username}</h1>
+        </Link>
+      ) : (
+        <Link className="link" to="/login">
+          <h1>Login/Signup</h1>
+        </Link>
+      )}
     </div>
   );
 };
