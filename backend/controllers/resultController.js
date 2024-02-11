64ab4d0ff4c1641ad28ad61e090b4a2ca430dd9f
Sushmita Ghosh <pujaghosh251096@gmail.com>
2024-02-11 04:00:34 -0800

    [Chingu Trivia] Result Routes, Result Controller, Model

---
+++ backend/controllers/resultController.js
@@ -0,0 +1,43 @@
+// result controller
+
+const Result = require("../models/result");
+
+async function handleFetchResults(req, res) {
+  const { email } = req.body;
+  if (!email) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
+  console.log(req.query);
+  const results = await Result.findOne({ email: email });
+  console.log(results);
+  res.json({ success: true, data: results });
+}
+
+async function handleSaveResults(req, res) {
+  const { email, score, category } = req.body;
+
+  if (!email || !score || !category) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
+
+  const result = await Result.findOne({ email });
+
+  if (result) {
+    await Result.updateOne({ email }, { $set: { score, category } });
+    res.json({ success: true, message: "Results saved successfully" });
+    return;
+  }
+
+  await Result.create({ email, score, category });
+
+  res.json({ success: true, message: "Results saved successfully" });
+}
+
+module.exports = {
+  handleFetchResults,
+  handleSaveResults,
+};
