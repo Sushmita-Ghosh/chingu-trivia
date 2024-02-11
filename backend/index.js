265afafb647420357be0ec88e83ba78cc631a509
Sushmita Ghosh <pujaghosh251096@gmail.com>
2024-02-11 04:01:08 -0800

    [Chingu Trivia] Backend Package Changes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const colors = require("colors");
 const userRouter = require("./routes/userRoutes");
+const resultRouter = require("./routes/resultRoutes");
 const connectDB = require("./connectDB");
 const cors = require("cors");
+const dotenv = require("dotenv").config();
 
 const app = express();
 
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/user", userRouter);
+app.use("/result", resultRouter);
 
 app.listen(5000, () => {
   console.log(`Server started on port 5000`.green.underline.bold);
